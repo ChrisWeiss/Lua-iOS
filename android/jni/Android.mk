f827ca0c70db9515b356a5751d1ef94f38517a46
Mark Pauley <pauley@anki.com>
2014-06-16 19:03:07 -0700

    Cleaning up print statements in the makefile

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -3,21 +3,20 @@ PROJECT_PATH:= $(shell (/usr/bin/git rev-parse --show-toplevel))
 
 # set local compilation path
 LOCAL_PATH:= $(PROJECT_PATH)/lua-5.2.2/src
-#$(warning $(LOCAL_PATH))
 
-# read list of c/cpp files
+# list of c/cpp files
 FILES := $(notdir $(wildcard $(LOCAL_PATH)/*.c))
-#$(warning $(FILES))
 
-# lua static lib
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := lua
 LOCAL_SRC_FILES := $(FILES)
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_CFLAGS := "-DLUA_USE_ANDROID"
-$(warning $(LOCAL_C_INCLUDES))
 
+#special luaconf.h directive..
+LOCAL_CFLAGS := "-DLUA_USE_ANDROID"
+
+# build lua static lib
 include $(BUILD_STATIC_LIBRARY)
 
 # lua shared lib
